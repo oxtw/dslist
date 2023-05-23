ddb0753d8c5acca310406ef5968f8d97acda08b7
oxtw <weigertmiguel@hotmail.com>
2023-05-23 14:52:52 -0300

    Homolog ok

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -28,7 +28,7 @@ public class GameMinDTO {
 
 		id = projection.getId();
 		title = projection.getTitle();
-		year = projection.getYear();
+		year = projection.getGameYear();
 		imgUrl = projection.getImgUrl();
 		shortDescription = projection.getShortDescription();
 	}
